2a2d5f72cca466c15afb83e7ec1101588ec00d09
sheikhabdulbasit-nickelfox <sheikh.abdulbasit@nickelfox.com>
2023-03-28 11:19:44 +0530

    feat(): app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AddTodo from './Components/AddTodo';
+import ListTodo from './Components/ListTodo';
+
+function App() {
+  return (
+    <div className="App">
+      <AddTodo />
+      <ListTodo />
+    </div>
+  );
+}
+
+export default App;
